6f14bb44285b119345733de0b356508dd1dd33bd
Roscoeriley <cortschwerdtfeger@yahoo.com>
2023-03-28 17:44:43 -0500

    Main screen done

--- app/src/main/java/com/example/seniorproject/MainActivity.kt
+++ app/src/main/java/com/example/seniorproject/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.seniorproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.seniorproject.data.DataSource.quantityOptions
 import com.example.seniorproject.ui.theme.SeniorProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MainScreen("Android")
+                    MainScreen(quantityOptions = quantityOptions, onNextButtonClicked = {})
                 }
             }
         }
@@ -30,14 +42,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(quantityOptions: List<Pair<Int, Int>>, onNextButtonClicked: (Int) -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(R.drawable.bowlero_logo),
+            contentDescription = "Bowlero Logo",
+            modifier = Modifier.width(300.dp))
+        Spacer(Modifier.height(16.dp))
+        quantityOptions.forEach { item ->
+            MainScreenButtons(
+                labelResourceId = item.first,
+                onClick = { onNextButtonClicked(item.second) }
+            )
+        }
+    }
+}
+
+@Composable
+fun MainScreenButtons(
+    @StringRes labelResourceId: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Button(
+        onClick = onClick,
+        modifier = modifier.widthIn(min = 250.dp)
+    ) {
+        Text(stringResource(labelResourceId))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SeniorProjectTheme {
-        MainScreen("Android")
+        MainScreen(quantityOptions = quantityOptions, onNextButtonClicked = {})
     }
 }
---
+++ app/src/main/java/com/example/seniorproject/data/DataSource.kt
@@ -0,0 +1,14 @@
+package com.example.seniorproject.data
+
+import com.example.seniorproject.R
+
+object DataSource {
+    val quantityOptions = listOf(
+        Pair(R.string.login, 1),
+        Pair(R.string.leagues, 2),
+        Pair(R.string.tournaments, 3),
+        Pair(R.string.statistics, 4),
+        Pair(R.string.settings, 5),
+        Pair(R.string.help, 6)
+    )
+}
